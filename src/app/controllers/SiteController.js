64188e0142e7d845650e951eb640062225800b63
VuBui <admin@MBP-cua-Admin.lan>
2023-04-05 03:36:27 +0700

    upload code

--- src/app/controllers/SiteController.js
+++ src/app/controllers/SiteController.js
@@ -4,15 +4,9 @@ const { listMongooseToObject } = require("../../util/mongoose");
 class SiteController {
   // [GET] /
   home(req, res, next) {
-    // res.render("home");
-
-    // Course.find({})
-    //   .then((courses) =>
-    //     // res.render("home", { courses: listMongooseToObject(courses) })
-    //     res.json(courses)
-    //   )
-    //   .catch(next);
-    res.render("home");
+    Course.find({})
+      .then((courses) => res.json(courses))
+      .catch(next);
   }
 
   // [GET] /search
